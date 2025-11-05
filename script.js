2a29d021232526fe094b9080777f2e95e072bb6f
Adyan <78735036+AdyanMidzic08@users.noreply.github.com>
2025-11-05 23:02:26 +0100

    Finished with UI-Index for now

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Hilfe von KI
-for (let i = 0; i < 25; i++) {
+/*for (let i = 0; i < 25; i++) {
     let star = document.createElement('div');
     star.classList.add('sparkle');
     star.style.top = Math.random() * 100 + 'vh';
     star.style.left = Math.random() * 100 + 'vw';
     star.style.animationDuration = 1.5 + Math.random() * 2 + 's';
     document.body.appendChild(star);
-}
+}*/
